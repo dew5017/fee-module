bbb3fe813b218928c5fb40f238576ebfa1d2534f
dew5017 <dewesh.tiwari@falconfs.com>
2023-02-15 19:41:28 +0530

    Initial commit for Fee Module

---
+++ src/test/java/in/kitecash/tab/feemodule/FeeModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package in.kitecash.tab.feemodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeeModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
